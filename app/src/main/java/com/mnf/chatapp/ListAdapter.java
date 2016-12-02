b2ff29117f70f54e580535b0d080c0d0c9ca3ae8
Muneef M <muneez4us@icloud.com>
2016-12-02 23:18:05 +0530

    fixes

---
+++ app/src/main/java/com/mnf/chatapp/ListAdapter.java
@@ -0,0 +1,50 @@
+package com.mnf.chatapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Muneef on 02/12/2016.
+ */
+
+public class ListAdapter extends ArrayAdapter<Model> {
+    public ListAdapter(Context context, int resource, List<Model> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.message_item, parent, false);
+        }
+
+        ImageView photoImageView = (ImageView) convertView.findViewById(R.id.photoImageView);
+        TextView messageTextView = (TextView) convertView.findViewById(R.id.messageTextView);
+        TextView authorTextView = (TextView) convertView.findViewById(R.id.nameTextView);
+
+        Model model = getItem(position);
+
+        boolean isPhoto = model.getPhotoUrl() != null;
+        if (isPhoto) {
+            messageTextView.setVisibility(View.GONE);
+            photoImageView.setVisibility(View.VISIBLE);
+            /*Glide.with(photoImageView.getContext())
+                    .load(model.getPhotoUrl())
+                    .into(photoImageView);*/
+        } else {
+            messageTextView.setVisibility(View.VISIBLE);
+            photoImageView.setVisibility(View.GONE);
+            messageTextView.setText(model.getText());
+        }
+        authorTextView.setText(model.getName());
+
+        return convertView;
+    }
+}
