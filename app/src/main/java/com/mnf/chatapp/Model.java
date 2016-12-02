6e7eb8c91a716691730bcdefedee10c0f6ad49da
Muneef M <muneez4us@icloud.com>
2016-12-02 22:58:12 +0530

    first commit

---
+++ app/src/main/java/com/mnf/chatapp/Model.java
@@ -0,0 +1,45 @@
+package com.mnf.chatapp;
+
+/**
+ * Created by Muneef on 02/12/2016.
+ */
+
+public class Model {
+   private String name;
+    private String text;
+    private String photoUrl;
+
+
+    public Model(String name,String text,String photoUrl){
+        this.name = name;
+        this.text = text;
+        this.photoUrl = photoUrl;
+    }
+
+
+    public String getName(){
+        return name;
+    }
+    public void  setName(String name){
+        this.name = name;
+    }
+
+
+    public String getText(){
+        return text;
+    }
+    public void  setText(String text){
+        this.text = text;
+    }
+
+
+    public String getPhotoUrl(){
+        return photoUrl;
+    }
+    public void  setPhotoUrl(String photoUrl){
+        this.photoUrl = photoUrl;
+    }
+
+
+
+}
